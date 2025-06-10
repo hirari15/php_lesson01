158af13e3987f5208e88cbac88d0170c0d0368b7
hirari <omochisann.15@gmail.com>
2025-06-10 23:01:49 +0900

    曜日取得

--- lesson01/sample10.php
+++ lesson01/sample10.php
@@ -1,8 +1,7 @@
 <?php
 $week_name = ['日', '月', '火', '水', '木', '金', '土'];
-echo $week_name[0] . '<br>';
 
 date_default_timezone_set('Asia/Tokyo');
 $week = date('w');
-echo $week;
+echo $week_name[$week];
 ?>
