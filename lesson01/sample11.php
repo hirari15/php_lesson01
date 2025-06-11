248e9b98c157101626d14d9c46eeaa2df4ebbdf6
hirari <omochisann.15@gmail.com>
2025-06-11 23:03:57 +0900

    foreach

--- lesson01/sample11.php
+++ lesson01/sample11.php
@@ -6,6 +6,9 @@ $fruits = [
     'tomato' => 'トマト',
     'peach' => 'もも'
 ];
-
-echo $fruits['apple'];
 ?>
+<dl>
+    <?php foreach ($fruits as $english => $japanese): ?>
+        <dt><?php echo $english; ?></dt>
+        <dd><?php echo $japanese; ?></dd>
+    <?php endforeach; ?>
