5b0caaf373b11af0048b3f3ad7bccabb39c6363c
hirari <omochisann.15@gmail.com>
2025-06-12 20:27:34 +0900

    sample12完成

--- lesson01/sample12.php
+++ lesson01/sample12.php
@@ -1,1 +1,14 @@
-<p>ようこそ</p>
+<?php
+date_default_timezone_set('Asia/Tokyo');
+$time = date('G');
+?>
+<?php if ($time < 9): ?>
+    <p>※ 営業時間外です</p>
+<?php else: ?>
+    <p>ようこそ</p>
+<?php endif; ?>
+<?php
+$s = '';
+if ($s): // $s !== ''  
+    echo '$sには文字が入っています';
+endif;
