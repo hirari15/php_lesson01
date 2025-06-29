fad5559baa7e40ce6806d44766b3168cfd23a549
hirari <omochisann.15@gmail.com>
2025-06-29 22:58:27 +0900

    sample19完成

--- lesson01/sample18.php
+++ lesson01/sample18.php
@@ -2,4 +2,9 @@
 $file =file_get_contents('feed.json');
 $json = json_decode($file);
 
-echo $json->title;
+foreach ($json->items as $item) :
+?>
+・<a href="<?php echo $item->url; ?>"><?php echo $item->title; ?></a><br>
+<?php
+endforeach;
+?>
