8229473093560e79b36530c1b1e233db67f45846
hirari <omochisann.15@gmail.com>
2025-07-02 23:07:39 +0900

    body更新

--- lesson01/sample21/submit.php
+++ lesson01/sample21/submit.php
@@ -7,6 +7,10 @@
 </head>
 <body>
     <h2>ご予約日</h2>
+    <ul>
+        <li>1/2</li>
+        <li>1/3</li>
+    <ul>
     <?php if (!empty($_REPUEST['reserve'])): ?>
     <?php $reserves = $_REQUEST['reserve']; ?>
     <ul>
