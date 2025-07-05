5a03c9924b3ccd76deb55559240a1e8018b200c9
hirari <omochisann.15@gmail.com>
2025-07-05 15:36:08 +0900

    表更新

--- lesson01/sample25.php
+++ lesson01/sample25.php
@@ -1,4 +1,15 @@
-<?php
-$answer = 10 % 3;
-echo $answer;
-?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sample25</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <td>1行目</td>
+        </tr>
+    </table>
+</body>
+</html>
